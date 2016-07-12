29881524b89a8e33ca20bcaa70d3ebe88508e00a
DianeD <ddiaz@microsoft.com>
2016-07-12 11:32:07 -0700

    tentant -> tenant

--- src/svcConsts/svcConsts.ts
+++ src/svcConsts/svcConsts.ts
@@ -1,5 +1,5 @@
 export class SvcConsts {
-	public static CLIENT_ID:string = "3ec6a43d-34e7-4710-8207-d0ee6e2b7300";
-	public static TENTANT_ID:string = "rzdemos.onmicrosoft.com";
+	public static CLIENT_ID:string = "ENTER_YOUR_CLIENT_ID";
+	public static TENANT_ID:string = "rzdemos.onmicrosoft.com";
 	public static GRAPH_RESOURCE:string = "https://graph.microsoft.com";
 }
